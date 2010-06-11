1d29f9bfc648ac5025e2b15f71523514a9d1b9f6
Gabriel Horner <gabriel.horner@gmail.com>
2010-06-11 00:19:21 -0400

    got rid of $: hacks

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,7 +3,6 @@ require 'test/unit'
 require 'context' #gem install jeremymcanally-context -s http://gems.github.com
 require 'mocha' #gem install mocha
 require 'matchy' #gem install jeremymcanally-matchy -s http://gems.github.com
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'alias'
 
 class Test::Unit::TestCase
